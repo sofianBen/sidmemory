f1bdddcde78022f48e38afedbcd9b5f1578f4862
Camille Roger <camille.rg31@gmail.com>
2017-03-26 18:57:55 +0200

    maj creation_partie_solo et creation_partie_multi

--- sql/procedures.sql
+++ sql/procedures.sql
@@ -231,7 +231,7 @@ END;
 --------------------------------------------------------------------------------------------------------------
 -- afficherNiveaux
 --------------------------------------------------------------------------------------------------------------
-create or replace function niveauJoueur(
+create or replace function niveau_joueur(
     pId_joueur in Joueur.id_joueur%type)
     return number AS
 
@@ -271,7 +271,7 @@ end ;
 --------------------------------------------------------------------------------------------------------------
 -- creationPartie pour 1 joueur
 --------------------------------------------------------------------------------------------------------------
-create or replace procedure creation_partie(
+create or replace procedure creation_partie_solo(
     pId_niveau in Niveau.id_niveau%TYPE,
     pId_joueur in Joueur.id_joueur%TYPE,
     rId_partie out Partie.id_partie%TYPE) AS
@@ -290,6 +290,66 @@ create or replace procedure creation_partie(
 BEGIN
   select seq_partie.nextval into rId_partie from dual;
   insert into Partie(id_partie, id_niveau, id_joueur) values(rId_partie, pId_niveau, pId_joueur);
+  -- après l'insert dans la table Partie, le trigger verification_defaites va être lancé tout seul
+
+  -- select pour avoir les nombres de lignes et les nombres de colonnes
+  select nb_ligne, nb_colonne into vNb_ligne, vNb_colonne from Niveau where id_niveau = pId_niveau;
+  
+  -- séléction de la collection en foncrtion du niveau
+  select id_collection into vCollection from Niveau
+  where id_niveau = pId_niveau;
+  
+  -- sélction de la première image de la collection
+  select min(id_image) into vMinId_image 
+  from Image where id_collection = vCollection;
+
+  -- stocker dans un tableau des nombres aléatoires en évitant les doublants
+  -- ces nombres seront piochés parmis les 'id_image' de la bonne collection
+  select distinct trunc(dbms_random.value(vMinId_image,vMinId_image+(vNb_ligne*vNb_colonne))) BULK COLLECT into tabId_image
+  from dual
+  connect by level <= 500
+  order by dbms_random.value;
+
+  -- boucle for pour créer les tuples de la table Carte
+  for ligne in 1..vNb_ligne loop
+    for colonne in 1..vNb_colonne loop
+      insert into Carte values (seq_carte.nextval, vNb_ligne, vNb_colonne, tabId_image(i), rId_partie); -- stocker la variable de retour à la place de mettre sql_partie.currval
+      i := i + 1;
+    End loop;
+  End loop;
+
+EXCEPTION -- à compléter
+  --when todo
+  when others then
+  dbms_output.put_line('Erreur inconnue '|| sqlcode || ' : '|| sqlerrm );
+
+END;
+/
+
+
+--------------------------------------------------------------------------------------------------------------
+-- creationPartie pour 2 joueurs
+--------------------------------------------------------------------------------------------------------------
+create or replace procedure creation_partie_multi(
+    pId_niveau in Niveau.id_niveau%TYPE,
+    pId_joueur in Joueur.id_joueur%TYPE,
+    pId_joueur2 in Joueur.id_joueur%TYPE,
+    rId_partie out Partie.id_partie%TYPE) AS
+
+  vCollection Collection.id_collection%TYPE;
+  nbCarte number := 0;
+  vMinId_image Image.id_image%TYPE; 
+  vNb_ligne Niveau.nb_ligne%TYPE;
+  vNb_colonne Niveau.nb_colonne%TYPE;
+  type typ_tab_random is varray(100) of number; -- déclaration d'un type 'tableau' de nombres aléatoires
+  tabId_image typ_tab_random; -- déclaration du tableau
+  ligne number := 0;
+  colonne number := 0;
+  i number := 1; -- rang/place courante dans le tableau (place qui correspondra à une carte par la suite)
+
+BEGIN
+  select seq_partie.nextval into rId_partie from dual;
+  insert into Partie(id_partie, id_niveau, id_joueur, id_joueur2) values(rId_partie, pId_niveau, pId_joueur, pId_joueur2);
   -- après l'insert dans la table Partie, le trigger verification_defaites va être lancé tout seul
 
   -- select pour avoir les nombres de lignes et les nombres de colonnes
