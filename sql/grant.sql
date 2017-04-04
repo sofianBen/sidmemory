7fb61c60add28bd6fa486bb2d13f9d54b56fa3be
Camille Roger <camille.rg31@gmail.com>
2017-04-04 17:40:16 +0200

    Maj grant

--- sql/grant.sql
+++ sql/grant.sql
@@ -1,21 +1,21 @@
 --------------------------------------------------------------------------------------------------------------
 -- Select
 --------------------------------------------------------------------------------------------------------------
-
-
-GRANT SELECT ON Partie TO sofian
-GRANT SELECT ON Joueur TO sofian
-
---------------------------------------------------------------------------------------------------------------
--- Update
---------------------------------------------------------------------------------------------------------------
-
-
-GRANT UPDATE (etat) ON TABLE Partie TO sofian
+GRANT SELECT ON Partie TO sofian;
+GRANT SELECT ON Joueur TO sofian;
+GRANT SELECT ON Carte TO sofian;
+GRANT SELECT ON Image TO sofian;
+GRANT SELECT ON Niveau TO sofian
+GRANT SELECT ON vue_highscore_jour TO sofian;
+GRANT SELECT ON vue_highscore_semaine TO sofian;
+GRANT SELECT ON vue_highscore_global TO sofian;
 
 --------------------------------------------------------------------------------------------------------------
--- execute
+-- Execute
 --------------------------------------------------------------------------------------------------------------
-
-
 GRANT EXECUTE ON inscription TO sofian;
+GRANT EXECUTE ON connexion TO sofian;
+GRANT EXECUTE ON niveau_joueur TO sofian;
+GRANT EXECUTE ON creation_partie_multi TO sofian;
+GRANT EXECUTE ON creation_partie_solo TO sofian;
+GRANT EXECUTE ON id_joueur_en_pseudo TO sofian;
