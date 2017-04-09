d43baf8a598e289d8553c6da8c44d435cb6df333
BENJEBRIA Sofian <sofian.benjebria@hotmail.fr>
2017-04-09 17:32:53 +0200

    correction

--- sql/grant.sql
+++ sql/grant.sql
@@ -1,21 +1,21 @@
 --------------------------------------------------------------------------------------------------------------
 -- Select
 --------------------------------------------------------------------------------------------------------------
-GRANT SELECT ON Partie TO '21401992';
-GRANT SELECT ON Joueur TO '21401992';
-GRANT SELECT ON Carte TO '21401992';
-GRANT SELECT ON Image TO '21401992';
-GRANT SELECT ON Niveau TO '21401992';
-GRANT SELECT ON vue_highscore_jour TO '21401992';
-GRANT SELECT ON vue_highscore_semaine TO '21401992';
-GRANT SELECT ON vue_highscore_global TO '21401992';
+GRANT SELECT ON Partie TO "21401992";
+GRANT SELECT ON Joueur TO "21401992";
+GRANT SELECT ON Carte TO "21401992";
+GRANT SELECT ON Image TO "21401992";
+GRANT SELECT ON Niveau TO "21401992";
+GRANT SELECT ON vue_highscore_jour TO "21401992";
+GRANT SELECT ON vue_highscore_semaine TO "21401992";
+GRANT SELECT ON vue_highscore_global TO "21401992";
 
 --------------------------------------------------------------------------------------------------------------
 -- Execute
 --------------------------------------------------------------------------------------------------------------
-GRANT EXECUTE ON inscription TO '21401992';
-GRANT EXECUTE ON connexion TO '21401992';
-GRANT EXECUTE ON niveau_joueur TO '21401992';
-GRANT EXECUTE ON creation_partie_multi TO '21401992';
-GRANT EXECUTE ON creation_partie_solo TO '21401992';
-GRANT EXECUTE ON id_joueur_en_pseudo TO '21401992';
+GRANT EXECUTE ON inscription TO "21401992";
+GRANT EXECUTE ON connexion TO "21401992";
+GRANT EXECUTE ON niveau_joueur TO "21401992";
+GRANT EXECUTE ON creation_partie_multi TO "21401992";
+GRANT EXECUTE ON creation_partie_solo TO "21401992";
+GRANT EXECUTE ON id_joueur_en_pseudo TO "21401992";
